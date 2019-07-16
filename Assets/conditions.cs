f9be3d57d85addf4ac79e08e9a32986b392af8d7
ashkanmoradi <asorrow1214@gmail.com>
2019-07-16 18:57:31 +0200

    init

---
+++ Assets/conditions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conditions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    private void Gamestop()
+    {
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Countdown(int seconds)
+    {
+        int counter = seconds;
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1);
+            print(counter + " seconds remaining");
+            counter--;
+        }
+        Gamestop();
+    }
+
+
+    private void OnTriggerEnter(Collider col)
+    
+    { 
+        StopCoroutine(Countdown(5));
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        StartCoroutine(Countdown(5));
+    }
+}
